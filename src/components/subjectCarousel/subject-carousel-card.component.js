e3174c14efc2f38714a3c43dc4ebbe39d78a6e77
Justin Graber <jgraberdev@gmail.com>
2017-01-29 07:13:59 -0600

    flag icon and finished subject carousel card layout

--- src/components/subjectCarousel/subject-carousel-card.component.js
+++ src/components/subjectCarousel/subject-carousel-card.component.js
@@ -3,6 +3,7 @@ import TwitterIcon from '../../icons/twitter.icon';
 import FacebookIcon from '../../icons/facebook.icon';
 import LinkedinIcon from '../../icons/linkedin.icon';
 import GooglePlusIcon from '../../icons/google-plus.icon';
+import FlagIcon from '../../icons/flag.icon';
 import './subject-carousel-card.component.css';
 
 class SubjectCarouselCard extends Component {
@@ -12,11 +13,21 @@ class SubjectCarouselCard extends Component {
         <div className="subjectCarouselCard-score">
           <div className="subjectCarouselCard-score_inner">100</div>
         </div>
+        <div className="subjectCarouselCard-flag">
+          <FlagIcon cssClass="subjectCarouselCard-flag_icon"/>
+        </div>
+
         <div className="subjectCarouselCard-profile"></div>
-        <TwitterIcon cssClass="svgIcon_medium" />
-        <FacebookIcon cssClass="svgIcon_medium" />
-        <LinkedinIcon cssClass="svgIcon_medium" />
-        <GooglePlusIcon cssClass="svgIcon_medium" />
+        <div className="subjectCarouselCard-metaInfo">
+          <div className="subjectCarouselCard-metaInfo_primary">John Jones</div>
+          <div className="subjectCarouselCard-metaInfo_secondary">1995</div>
+        </div>
+        <div className="subjectCarouselCard-social">
+          <TwitterIcon cssClass="svgIcon_medium" />
+          <FacebookIcon cssClass="svgIcon_medium" />
+          <LinkedinIcon cssClass="svgIcon_medium" />
+          <GooglePlusIcon cssClass="svgIcon_medium" />
+        </div>
       </div>
     )
   }
